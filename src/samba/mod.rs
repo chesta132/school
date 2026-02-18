84438def6ab28095fe78b60c1340f3ed42a18de4
Chesta Ardiona <chestapanduganteng@gmail.com>
2026-02-18 19:12:39 +0700

    chore remove option in samba run

--- src/samba/mod.rs
+++ src/samba/mod.rs
@@ -3,29 +3,12 @@ mod types;
 
 pub use types::*;
 
-use crate::{
-    cmd::{Prompt, install_pkg},
-    error,
-    log::{log_invalid_choice, log_section, log_step},
-};
+use crate::{cmd::install_pkg, error, log::log_step};
 
 pub fn run() -> error::Result<(String, Vec<(&'static str, String)>)> {
-    let mut prompt = Prompt::new();
     install_pkg(&vec!["samba"], "bind9", "install samba", || {
         log_step("Installing", "samba")
     })?;
 
-    loop {
-        log_section("Samba Configuration");
-        println!("   1  share");
-        println!();
-
-        let input = prompt.readline("❯ ");
-
-        match input.as_str() {
-            "1" => return share::share(),
-            "q" | "Q" => return Ok(("Cancelled".to_string(), vec![])),
-            _ => log_invalid_choice(),
-        }
-    }
+    share::share()
 }
