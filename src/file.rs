4644064c3997a20969ec1661102ba92d1835c2fa
Chesta Ardiona <chestapanduganteng@gmail.com>
2026-02-19 12:12:49 +0700

    refactor: use create method in open_with_append_or_create

--- src/file.rs
+++ src/file.rs
@@ -1,15 +1,25 @@
-use std::{fs::{File, OpenOptions}, io::Read};
+use std::{
+    fs::{File, OpenOptions},
+    io::Read,
+};
+
+use crate::error::{self, Error};
 
 pub fn open_with_append_or_create(path: &str) -> File {
     OpenOptions::new()
         .read(true)
         .append(true)
+        .create(true)
         .open(path)
-        .unwrap_or_else(|_| File::create(path).unwrap())
+        .unwrap()
 }
 
-pub fn read_file(file: &mut File) -> String {
+pub fn read_file(file: &mut File, while_do: &'static str) -> error::Result<String> {
     let mut content = String::new();
-    file.read_to_string(&mut content).unwrap();
-    content
+    file.read_to_string(&mut content).map_err(|err| Error {
+        error: vec![Box::new(err)],
+        error_on: "read_file",
+        error_while: while_do,
+    })?;
+    Ok(content)
 }
